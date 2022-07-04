00470dd52c606f8fc50a2e442015dfc8653914dd
Test User <testuser@example.com>
2022-07-04 17:51:05 +0300

    Calculate x value and click Submit

---
+++ python_lesson2.1_step5.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x_element = browser.find_element(By.CSS_SELECTOR, ".container .form-group #input_value")
+    x = x_element.text
+    y = calc(x)
+    text_field = browser.find_element(By.CSS_SELECTOR, "#answer")
+    text_field.send_keys(y)
+    checkbox = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    checkbox.click()
+    radiobutton = browser.find_element(By.CSS_SELECTOR, "[value='robots']")
+    radiobutton.click()
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
